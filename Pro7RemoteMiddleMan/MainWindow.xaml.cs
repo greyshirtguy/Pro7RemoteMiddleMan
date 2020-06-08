58bf47fd4be6a5320faf001eab2c09de2ad4b124
Dan Owen <greyshirtguy@gmail.com>
2020-06-08 17:31:55 +1000

    Made app into a usable MVP.

--- Pro7RemoteMiddleMan/MainWindow.xaml.cs
+++ Pro7RemoteMiddleMan/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace Pro7RemoteMiddleMan
     {
         WebSocket MasterWebSocket; // WebSocket connection to Pro7 (treated as "Master")
         WebSocket SlaveWebSocket; // WebSocket connection to Pro7 (treated as "Slave")
+        String MasterSettings = ""; // Keeping track of settings used to connect (so we can re-connect if they change)
+        String SlaveSettings = ""; // Keeping track of settings used to connect (so we can re-connect if they change)
         String CurrentPresentationPath;  // This is kept as part of the logic for a work-around to avoid hanging Pro7 (Never send a presentationTriggerIndex message that targets a presentation that is not currently active without first sending a presentationRequest message!)
 
         //websocket.Send("{\"action\":\"presentationTriggerNext\"}");
@@ -36,29 +38,70 @@ namespace Pro7RemoteMiddleMan
         #region Window Events
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // Open WebSocket connections once window is loaded.
+            // MoveFocus controls to suit window size
+            UpdateControlsLayout();
 
-            // TODO:
-            // Update UI when connected/disconnected.
-            // Add UI to set IP address/host and password.
-            // Add code to save in settings
-            // Add logic to watch connection and try to reconnect in background if connection fails    
+            //  watchDogTimer will check connections once per second and try to re-connect if not connected.
+            System.Windows.Threading.DispatcherTimer watchDogTimer = new System.Windows.Threading.DispatcherTimer();
+            watchDogTimer.Tick += WatchDogTimer_Tick;
+            watchDogTimer.Interval = new TimeSpan(0, 0, 1);
+            watchDogTimer.Start();
+
+            // TODO: 
             // "Flicker" indicators (and/or) arrows when messages are coming/going?
 
-            MasterWebSocket = new WebSocket("ws://127.0.0.1:55556/remote");
-            MasterWebSocket.Opened += MasterWebSocket_Opened;
-            MasterWebSocket.Closed += MasterWebSocket_Closed;
-            MasterWebSocket.Error += MasterWebSocket_Error;
-            MasterWebSocket.MessageReceived += MasterWebSocket_MessageReceived;
-            MasterWebSocket.Open();
+
+        }
+
+        private void WatchDogTimer_Tick(object sender, EventArgs e)
+        {
+            // TODO: Consider putting in an exit clause here to avoid re-connecting until 2-3 seconds after editing settings??
+
+            // If settings have changed or we are not connected then (re)establish Master connection
+            if (MasterWebSocket == null || (MasterWebSocket.State == WebSocketState.Closed || MasterWebSocket.State == WebSocketState.None || MasterSettings != Properties.Settings.Default.MasterNetworkAddress + Properties.Settings.Default.MasterNetworkPort + Properties.Settings.Default.MasterPassword))
+            {
+                // Kill existing connection (in case where properties have changed but we have an open connection)
+                if (MasterWebSocket != null && MasterWebSocket.State == WebSocketState.Open)
+                {
+                    MasterWebSocket.Close();
+                    MasterWebSocket.Opened -= MasterWebSocket_Opened;
+                    MasterWebSocket.Closed -= MasterWebSocket_Closed;
+                    MasterWebSocket.Error -= MasterWebSocket_Error;
+                    MasterWebSocket = null;
+                    
+                }
+                    
+                MasterWebSocket = new WebSocket("ws://" + Properties.Settings.Default.MasterNetworkAddress + ":" + Properties.Settings.Default.MasterNetworkPort + "/remote");
+                MasterWebSocket.Opened += MasterWebSocket_Opened;
+                MasterWebSocket.Closed += MasterWebSocket_Closed;
+                MasterWebSocket.Error += MasterWebSocket_Error;
+                MasterWebSocket.MessageReceived += MasterWebSocket_MessageReceived;
+                MasterWebSocket.Open();
+                MasterSettings = Properties.Settings.Default.MasterNetworkAddress + Properties.Settings.Default.MasterNetworkPort + Properties.Settings.Default.MasterPassword;
+            }
 
 
-            SlaveWebSocket = new WebSocket("ws://192.168.1.61:55555/remote");
-            SlaveWebSocket.Opened += SlaveWebSocket_Opened;
-            SlaveWebSocket.Closed += SlaveWebSocket_Closed;
-            SlaveWebSocket.Error += SlaveWebSocket_Error;
-            SlaveWebSocket.MessageReceived += SlaveWebSocket_MessageReceived;
-            SlaveWebSocket.Open();
+            // If settings have changed or we are not connected then (re)establish Slave connection
+            if (SlaveWebSocket == null  || (SlaveWebSocket.State == WebSocketState.Closed || SlaveWebSocket.State == WebSocketState.None || SlaveSettings != Properties.Settings.Default.SlaveNetworkAddress + Properties.Settings.Default.SlaveNetworkPort + Properties.Settings.Default.SlavePassword))
+            {
+                // Kill existing connection (in case where properties have changed but we have an open connection)
+                if (SlaveWebSocket != null && SlaveWebSocket.State == WebSocketState.Open)
+                {
+                    SlaveWebSocket.Close();
+                    SlaveWebSocket.Opened -= SlaveWebSocket_Opened;
+                    SlaveWebSocket.Closed -= SlaveWebSocket_Closed;
+                    SlaveWebSocket.Error -= SlaveWebSocket_Error;
+                    SlaveWebSocket = null;
+                }
+
+                SlaveWebSocket = new WebSocket("ws://" + Properties.Settings.Default.SlaveNetworkAddress + ":" + Properties.Settings.Default.SlaveNetworkPort + "/remote");
+                SlaveWebSocket.Opened += SlaveWebSocket_Opened;
+                SlaveWebSocket.Closed += SlaveWebSocket_Closed;
+                SlaveWebSocket.Error += SlaveWebSocket_Error;
+                SlaveWebSocket.MessageReceived += SlaveWebSocket_MessageReceived;
+                SlaveWebSocket.Open();
+                SlaveSettings = Properties.Settings.Default.SlaveNetworkAddress + Properties.Settings.Default.SlaveNetworkPort + Properties.Settings.Default.SlavePassword;
+            }
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -66,6 +109,41 @@ namespace Pro7RemoteMiddleMan
             if (e.LeftButton == MouseButtonState.Pressed)
                 this.DragMove();
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnToggleSize_Click(object sender, RoutedEventArgs e)
+        {
+            // Toggle Window Size between small and large to hide/reveal extra controls.
+            if (this.Width > 400) // I  know - it's a crude way to check if the window is small or large..
+            {
+                // Set to SMALL window size
+                this.Width = 185;
+                this.Height = 40;
+
+                UpdateControlsLayout();
+
+            }
+            else
+            {
+                // Set to LARGE window size
+                this.Width = 470;
+                this.Height = 370;
+
+                UpdateControlsLayout();
+
+            }
+
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
         #endregion
 
         #region WebSocket Events
@@ -75,17 +153,22 @@ namespace Pro7RemoteMiddleMan
             Dispatcher.Invoke(() =>
             {
                 MasterConnectionIndicatorRectangle.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+                AddLog("MasterWebSocket Connected:");
             });
 
-            System.Diagnostics.Debug.Print("MasterWebSocket Connected:");
+            
 
             // Send authentication message
-            MasterWebSocket.Send("{\"action\":\"authenticate\",\"protocol\":\"700\",\"password\":\"control\"}"); // TODO: Use password from application settings/UI
+            MasterWebSocket.Send("{\"action\":\"authenticate\",\"protocol\":\"700\",\"password\":\"" + Properties.Settings.Default.MasterPassword  + "\"}");
         }
 
         private void MasterWebSocket_MessageReceived(object sender, MessageReceivedEventArgs e)
         {
-            System.Diagnostics.Debug.Print("MasterWebSocket Received Message: "  + e.Message);
+            Dispatcher.Invoke(() =>
+            {
+                AddLog("MasterWebSocket Received Message: " + e.Message); ;
+            });
+            
 
             // Get Dictionary representation of JSON message object objects with string keys
             var JSONObjMessage = new JavaScriptSerializer().Deserialize<Dictionary<string, object>>(e.Message);
@@ -115,7 +198,7 @@ namespace Pro7RemoteMiddleMan
                             if (SlaveWebSocket.State == WebSocketState.Open)
                             {
                                 slaveTriggercommand = "{\"action\":\"presentationTriggerIndex\",\"slideIndex\":\"" + slideIndex + "\",\"presentationPath\":\"" + presentationPath + "\"}";
-                                System.Diagnostics.Debug.Print("Socket 2 Sending: " + slaveTriggercommand);
+                                AddLog("Socket 2 Sending: " + slaveTriggercommand);
                                 // ALERT: to avoid Pro7 hanging/crash we do a workaround:
                                 // Always make sure if we are about to trigger a slide for a NEW presentationPath then send presentationRequest FIRST before presentationTriggerIndex
                                 // We do this by recording the currentPresentationPath and comparing before every triggered slide.
@@ -155,9 +238,10 @@ namespace Pro7RemoteMiddleMan
             Dispatcher.Invoke(() =>
             {
                 MasterConnectionIndicatorRectangle.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                AddLog("MasterWebSocket Error: " + e.Exception.Message);
             });
             
-            System.Diagnostics.Debug.Print("MasterWebSocket Error: " + e.Exception.Message);
+            
         }
 
         private void MasterWebSocket_Closed(object sender, EventArgs e)
@@ -168,9 +252,10 @@ namespace Pro7RemoteMiddleMan
             Dispatcher.Invoke(() =>
             {
                 MasterConnectionIndicatorRectangle.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                AddLog("MasterWebSocket Closed:");
             });
 
-            System.Diagnostics.Debug.Print("MasterWebSocket Closed:");
+            
         }
 
 
@@ -180,17 +265,22 @@ namespace Pro7RemoteMiddleMan
             Dispatcher.Invoke(() =>
             {
                 SlaveConnectionIndicatorRectangle.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+                AddLog("SlaveWebSocket Connected:");
             });
 
-            System.Diagnostics.Debug.Print("SlaveWebSocket Connected:");
+            
 
             // Send authentication message
-            SlaveWebSocket.Send("{\"action\":\"authenticate\",\"protocol\":\"700\",\"password\":\"control\"}"); // TODO: Use password from application settings/UI
+            SlaveWebSocket.Send("{\"action\":\"authenticate\",\"protocol\":\"700\",\"password\":\"" + Properties.Settings.Default.SlavePassword  + "\"}");
         }
 
         private void SlaveWebSocket_MessageReceived(object sender, MessageReceivedEventArgs e)
         {
-            System.Diagnostics.Debug.Print("SlaveWebSocket Received Message: " + e.Message);
+            Dispatcher.Invoke(() =>
+            {
+                AddLog("SlaveWebSocket Received Message: " + e.Message);
+            });
+            
             
             // Get Dictionary representation of JSON message object objects with string keys
             var JSONObjMessage = new JavaScriptSerializer().Deserialize<Dictionary<string, object>>(e.Message.ToString());
@@ -233,9 +323,10 @@ namespace Pro7RemoteMiddleMan
             Dispatcher.Invoke(() =>
             {
                 SlaveConnectionIndicatorRectangle.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                AddLog("SlaveWebSocket Error: " + e.Exception.Message);
             });
 
-            System.Diagnostics.Debug.Print("SlaveWebSocket Error: " + e.Exception.Message);
+            
         }
 
         private void SlaveWebSocket_Closed(object sender, EventArgs e)
@@ -246,11 +337,123 @@ namespace Pro7RemoteMiddleMan
             Dispatcher.Invoke(() =>
             {
                 SlaveConnectionIndicatorRectangle.Fill = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                AddLog("SlaveWebSocket Closed:");
             });
 
-            System.Diagnostics.Debug.Print("SlaveWebSocket Closed:");
+            
         }
         #endregion
 
+        private void AddLog(String logString)
+        {
+            txtLog.AppendText(logString + System.Environment.NewLine);
+            txtLog.Text = txtLog.Text.Last(500000);  // Let's only keep the last 500KB of log text.
+            txtLog.ScrollToEnd();
+        }
+
+        private void UpdateControlsLayout()
+        {
+            if (this.Width > 400)
+            {
+                // Move around the status indicators to suit LARGE window size
+                Thickness margin;
+                // Move the M:
+                margin = txtM.Margin;
+                margin.Left = 91;
+                margin.Top = 4;
+                txtM.Margin = margin;
+                // Move the Master indicator
+                margin = MasterConnectionIndicatorRectangle.Margin;
+                margin.Left = 124;
+                margin.Top = 15;
+                MasterConnectionIndicatorRectangle.Margin = margin;
+                // Move the arrow text
+                margin = txtArrows.Margin;
+                margin.Left = 213;
+                margin.Top = 2;
+                txtArrows.Margin = margin;
+                // Move the S:
+                margin = txtS.Margin;
+                margin.Left = 331;
+                margin.Top = 4;
+                txtS.Margin = margin;
+                // Move the Slave indicator
+                margin = SlaveConnectionIndicatorRectangle.Margin;
+                margin.Left = 357;
+                margin.Top = 15;
+                SlaveConnectionIndicatorRectangle.Margin = margin;
+            }
+            else
+            {
+                // Move around the status indicators to suit SMALL window size
+                Thickness margin;
+                // Move the M:
+                margin = txtM.Margin;
+                margin.Left = 5;
+                margin.Top = 0;
+                txtM.Margin = margin;
+                // Move the Master indicator
+                margin = MasterConnectionIndicatorRectangle.Margin;
+                margin.Left = 35;
+                margin.Top = 11;
+                MasterConnectionIndicatorRectangle.Margin = margin;
+                // Move the arrow text
+                margin = txtArrows.Margin;
+                margin.Left = 55;
+                margin.Top = -2;
+                txtArrows.Margin = margin;
+                // Move the S:
+                margin = txtS.Margin;
+                margin.Left = 100;
+                margin.Top = 0;
+                txtS.Margin = margin;
+                // Move the Slave indicator
+                margin = SlaveConnectionIndicatorRectangle.Margin;
+                margin.Left = 120;
+                margin.Top = 11;
+                SlaveConnectionIndicatorRectangle.Margin = margin;
+            }
+
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // A double click near top of window = toggle window size (just call the toogle button click)
+            if (e.LeftButton == MouseButtonState.Pressed && e.GetPosition(this).Y < 50)
+                btnToggleSize_Click(null, null);
+        }
+    }
+
+
+
+    public static class StringExtension
+    {
+        public static string Last(this string source, int tail_length)
+        {
+            if (tail_length >= source.Length)
+                return source;
+            return source.Substring(source.Length - tail_length);
+        }
+    }
+
+    public class SettingBindingExtension : Binding
+    {
+        public SettingBindingExtension()
+        {
+            Initialize();
+        }
+
+        public SettingBindingExtension(string path)
+            : base(path)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.Source = Properties.Settings.Default;
+            this.Mode = BindingMode.TwoWay;
+            this.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+        }
     }
 }
--- Pro7RemoteMiddleMan/Properties/Settings.Designer.cs
+++ Pro7RemoteMiddleMan/Properties/Settings.Designer.cs
@@ -8,22 +8,138 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Pro7RemoteMiddleMan.Properties
-{
-
-
+namespace Pro7RemoteMiddleMan.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double WindowLeft {
+            get {
+                return ((double)(this["WindowLeft"]));
+            }
+            set {
+                this["WindowLeft"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public double WindowTop {
+            get {
+                return ((double)(this["WindowTop"]));
+            }
+            set {
+                this["WindowTop"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("470")]
+        public double WindowWidth {
+            get {
+                return ((double)(this["WindowWidth"]));
+            }
+            set {
+                this["WindowWidth"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("370")]
+        public string WindowHeight {
+            get {
+                return ((string)(this["WindowHeight"]));
+            }
+            set {
+                this["WindowHeight"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("localhost")]
+        public string MasterNetworkAddress {
+            get {
+                return ((string)(this["MasterNetworkAddress"]));
+            }
+            set {
+                this["MasterNetworkAddress"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("65432")]
+        public string MasterNetworkPort {
+            get {
+                return ((string)(this["MasterNetworkPort"]));
+            }
+            set {
+                this["MasterNetworkPort"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("control")]
+        public string MasterPassword {
+            get {
+                return ((string)(this["MasterPassword"]));
+            }
+            set {
+                this["MasterPassword"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("localhost")]
+        public string SlaveNetworkAddress {
+            get {
+                return ((string)(this["SlaveNetworkAddress"]));
+            }
+            set {
+                this["SlaveNetworkAddress"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("65432")]
+        public string SlaveNetworkPort {
+            get {
+                return ((string)(this["SlaveNetworkPort"]));
+            }
+            set {
+                this["SlaveNetworkPort"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("control")]
+        public string SlavePassword {
+            get {
+                return ((string)(this["SlavePassword"]));
+            }
+            set {
+                this["SlavePassword"] = value;
             }
         }
     }
